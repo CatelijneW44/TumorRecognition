d4fec6836064519645da36e7609aaf22714268a9
Cate <24cwessels@pinewood.edu>
2025-02-08 15:35:55 -0800

    voxel to mm function
    translate voxel position to mm (explaination above func)

--- TumorRecognition.m
+++ TumorRecognition.m
@@ -1,6 +1,14 @@
 %nibabel library not needed for niftiread
 
-% opening files AND extracting/reading data
+% returns data type struct - used for converting to mm
+FLAIRInfo = niftiinfo("UPENN-GBM-00003_11_FLAIR.nii.gz");
+T1Info = niftiinfo("UPENN-GBM-00003_11_T1.nii.gz");
+T1GDInfo = niftiinfo("UPENN-GBM-00003_11_T1GD.nii.gz");
+T2Info = niftiinfo("UPENN-GBM-00003_11_T2.nii.gz");
+segmInfo = niftiinfo("UPENN-GBM-00003_11_segm.nii.gz");
+
+
+% opening files AND extracting/reading data <- return data type int16
 FLAIR = niftiread('UPENN-GBM-00003_11_FLAIR.nii.gz');
 T1 = niftiread("UPENN-GBM-00003_11_T1.nii.gz");
 T1GD = niftiread("UPENN-GBM-00003_11_T1GD.nii.gz");
@@ -16,3 +24,14 @@ segm = niftiread("UPENN-GBM-00003_11_segm.nii.gz");
 %choosing a point
 value = FLAIR(113, 142, :); %coordinates 113, 142 at each of the slices
 %disp(value) %returns intensity
+
+
+%converting to mm [width - left to right, height - top to bottom, depth -
+%slice #]
+mm = voxelToReal(FLAIRInfo, 113, 142, 100);
+disp("display mm " + mm);
+
+function mm = voxelToReal(image, X, Y, Z)
+    voxelSize = image.PixelDimensions;
+    mm = [X, Y, Z] .* voxelSize;
+end
